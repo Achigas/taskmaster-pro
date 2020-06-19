65ea45561f5f27c404af73788e5ab47d2bd4acee
AChigas <Achigas34@gmail.com>
2020-06-19 12:48:41 -0700

    Drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,54 @@ var loadTasks = function() {
   });
 };
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  update: function(event) {
+    var tempArr = [];
+
+    //loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+       
+     // add task dat to the temp array as an object
+     tempArr.push({
+       text: text,
+       date: date
+     });    
+    });
+  
+
+      //trim down lists's ID to match object property
+      var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  }
+});
+
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
