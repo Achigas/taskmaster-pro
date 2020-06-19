bd50c6c045206eae8e3bba0f614b2febac70f123
AChigas <Achigas34@gmail.com>
2020-06-19 16:38:32 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,6 +55,22 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
+  activate: function(event) {
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
+  },
+  deactivate: function(event) {
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
+  },
+  over: function(event) {
+     $(event).addClass("dropover-active")
+     $(".bottom-trash").addClass("bottom-trash-active")
+  },
+  out: function(event) {
+    $(event).removeClass("dropover-active")
+    $(".bottom-trash").removeClass("bottom-trash-active")
+  },
   update: function(event) {
     var tempArr = [];
 
@@ -226,6 +242,13 @@ $(".list-group").on("change", "input[type='text']", function() {
     auditTask($(taskSpan).closest(".list-group-item"));
 });
 
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
+
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
@@ -239,7 +262,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
